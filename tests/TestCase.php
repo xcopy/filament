4050cf94ffc3ffbb6eda12e3f6e4084047afd4ec
Kairat Jenishev <kairat.jenishev@gmail.com>
2026-02-20 13:34:15 +0600

    Remove placeholders and scaffold Filament package

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,37 +1,16 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Jenishev\Filament\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Jenishev\Filament\FilamentServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
-            SkeletonServiceProvider::class,
+            FilamentServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/../database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
     }
 }
